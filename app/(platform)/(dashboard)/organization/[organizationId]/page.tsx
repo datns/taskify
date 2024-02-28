397ba3f1c1d6ad527af3720fdb37d68f0791472c
dat.s.nguyen <dat.s.snguyen@taptap.com.vn>
2024-02-28 17:26:50 +0700

    build organization id page

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,21 +1,19 @@
-import {db} from "@/lib/db";
 import {
-	Board
-} from "@/app/(platform)/(dashboard)/organization/[organizationId]/board";
+	Info
+} from "@/app/(platform)/(dashboard)/organization/[organizationId]/_components/info";
+import {Separator} from "@/components/ui/separator";
 import {
-	Form
-} from "@/app/(platform)/(dashboard)/organization/[organizationId]/form";
+	BoardList
+} from "@/app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list";
 
 const OrganizationIdPage = async () => {
-	const boards = await db.board.findMany();
 
 	return (
-		<div className="flex flex-col">
-			<Form />
-			<div className="space-y-2">
-				{boards.map(board => (
-					<Board title={board.title} id={board.id} key={board.id} />
-				))}
+		<div className="w-full mb-20">
+			<Info />
+			<Separator className="my-4"/>
+			<div className="px-2 md:px-4">
+				<BoardList />
 			</div>
 		</div>
 	)
